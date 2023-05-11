9b2180c571a0e0ae23605281cf7b02e5c3de42e4
Peter Ogechi <pogechi031@gmail.com>
2023-05-11 05:20:45 -0400

    Python Programming

--- 0x01-python-if_else_loops_funtions/5-print_comb2.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for i in range(100):
-    print(f"{i:02d}", end=", ")
-    print(end="")
--- 0x01-python-if_else_loops_funtions/9-print_last_digit.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def print_last_digit(number):
-    result = str(number)
-    print(f"{result[-1]}" , end = "")
-    return result[-1]
