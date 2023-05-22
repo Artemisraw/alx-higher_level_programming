6a01841092f04286b2e8f93eddaef748dd2f3163
Peter Ogechi <pogechi031@gmail.com>
2023-05-22 05:37:24 -0400

    format error fixx

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    x = len(my_list)
-
-    while x >= 1:
-        x = x - 1
-        print("{:d}".format(my_list[x]))
+    if my_list ==  None:
+        return
+    else:
+        x = len(my_list)
+        while x >= 1:
+            x = x - 1
+            print("{:d}".format(my_list[x]))
