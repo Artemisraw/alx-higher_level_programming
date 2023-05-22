2a57b3ca73171fa86be20d85448551c70e4d5f98
Peter Ogechi <pogechi031@gmail.com>
2023-05-22 06:04:46 -0400

    format error fixx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[{}]):
+    if matrix is None:
+        return
     for x in matrix:
         for i, value in enumerate(x):
             if i == len(x) - 1:
