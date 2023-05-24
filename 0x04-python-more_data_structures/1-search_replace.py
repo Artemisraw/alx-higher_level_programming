caf556a4f903c293ae85b1a52a82228e6af92a52
Peter Ogechi <pogechi031@gmail.com>
2023-05-24 05:49:05 -0400

    Search and replacex

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,6 +7,6 @@ def search_replace(my_list, search, replace):
         new_list.append(x)
     for x in new_list:
         if x is search:
+            arr = x - 1
             new_list[arr] = replace
-        arr += 1
     return new_list
