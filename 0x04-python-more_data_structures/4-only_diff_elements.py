f40af544c6dfedbe2c3216a165ed334c4927e401
Peter Ogechi <pogechi031@gmail.com>
2023-05-25 05:35:34 -0400

    Only diffrentsx

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    unique_elements = {elem for elem in set_1 if elem not in set_2} | {elem for elem in set_2 if elem not in set_1}
+    return unique_elements
+
