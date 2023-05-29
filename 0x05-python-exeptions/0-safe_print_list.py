5e40e285f1cbe6462b404ccd2a9f37fd7ad42a50
Peter Ogechi <pogechi031@gmail.com>
2023-05-29 04:55:07 -0400

    Safe list printing

---
+++ 0x05-python-exeptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        print("\n")
+        return count
+    except IndexError:
+        count = 0
+        for _ in my_list:
+            count += 1
+        return count
