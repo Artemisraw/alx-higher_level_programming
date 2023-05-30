42b9c4dce7915214d1f29b2db3a57b8c16345c8e
Peter Ogechi <pogechi031@gmail.com>
2023-05-30 14:31:38 -0400

    My first square classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 """ Creating a class square"""
+
 class Square:
-    """
-    This is just an empty class Square
-    """
+    """This is just an empty class Square to represent a square."""
     pass
