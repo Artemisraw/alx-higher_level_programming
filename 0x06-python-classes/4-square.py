f34f3c09effdfea9ef11c82e9dfa8fed2e4b2ee2
Peter Ogechi <pogechi031@gmail.com>
2023-05-30 21:42:26 -0400

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""Defining a class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initializing the clsaa
+
+        Args:
+        size(int) size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the method size """
+
+        if type(value) != int:
+            """ Checks if the size is an integer"""
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            """Checks if size is less than 0"""
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Method that returns the are of the Square"""
+        area = self.__size * self.__size
+
+        return area
