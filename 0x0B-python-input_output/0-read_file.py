76b86dc044550efc4f10c7dd6859f36393e24195
Artemisraw <ogp1875@gmail.com>
2023-11-08 00:25:40 -0500

    Python Object quickfix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+# This is a module that is used to read content from a file specified as an argument filename
 def read_file(filename=""):
     with open(filename , mode='r', encoding="utf-8") as myfile:
+
+        # The funtion open() is used to open the file in the read format and with the encoding of uft-8
+
         print(myfile.read())
-        
+    # After the reading operation is done close the file    
     myfile.close()
